416b00f36bffe5a985e0d6f726c7523aaf832da9
Massimo <mambrosi98@gmail.com>
2025-04-20 18:07:54 +0200

    add api client to fetch board and commands

---
+++ src/main/java/com/github/hangovers/client/ApiClient.java
@@ -0,0 +1,68 @@
+package com.github.hangovers.client;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.hangovers.model.Board;
+import com.github.hangovers.model.CommandsList;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.time.Duration;
+
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
+
+public class ApiClient {
+
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    private static final String BOARD_API = System.getenv("BOARD_API");
+    private static final String COMMANDS_API = System.getenv("COMMANDS_API");
+
+    public ApiClient() {
+        this.httpClient = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
+
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public Board fetchBoard() throws URISyntaxException, IOException, InterruptedException {
+        var request = HttpRequest.newBuilder()
+                .uri(new URI(BOARD_API))
+                .header("Accept","application/json")
+                .GET()
+                .build();
+
+        var response = httpClient.send(request,
+                ofString());
+
+        if (response.statusCode() != 200) {
+            throw new IOException("Failed to fetch board: HTTP status code " + response.statusCode());
+        }
+
+        return objectMapper.readValue(response.body(), Board.class);
+    }
+
+    public CommandsList fetchCommands() throws URISyntaxException, IOException, InterruptedException {
+        var request = HttpRequest.newBuilder()
+                .uri(new URI(COMMANDS_API))
+                .header("Accept","application/json")
+                .GET()
+                .build();
+
+        var response = httpClient.send(request,
+                ofString());
+
+        if (response.statusCode() != 200) {
+            throw new IOException("Failed to fetch commands: HTTP status code " + response.statusCode());
+        }
+
+        return objectMapper.readValue(response.body(), CommandsList.class);
+    }
+
+
+}
---
+++ src/main/java/com/github/hangovers/model/CommandsList.java
@@ -0,0 +1,6 @@
+package com.github.hangovers.model;
+
+import java.util.List;
+
+public record CommandsList(List<String> commands) {
+}
